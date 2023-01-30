8f6597443d0e618f05ff6a706c84130382823266
Adeel ahmed Munsha <117924594+Adeel102143@users.noreply.github.com>
2023-01-30 23:51:49 +0300

    via personal computer

---
+++ main.js
@@ -0,0 +1,14 @@
+
+   function myFunction() {
+    alert("Mighty checken burger");
+   }
+
+
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
